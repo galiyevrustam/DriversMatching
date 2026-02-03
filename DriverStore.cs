4ccfc71efc20ec30af1792e501935622e7e97416
galiyevrustam <ourheroe15@gmail.com>
2026-02-03 13:15:27 +0300

    добавлены модели Position, Driver, DriverStore и базовый вывод списка водителей

---
+++ DriverStore.cs
@@ -0,0 +1,33 @@
+﻿namespace DriverMatching;
+
+public class DriverStore
+{
+    private readonly Dictionary<string, Driver> _drivers = new();
+
+    public void AddOrUpdate(string id, int x, int y)
+    {
+        var position = new Position(x, y);
+
+        if (_drivers.TryGetValue(id, out var existing))
+        {
+            existing.UpdatePosition(position);
+        }
+        else
+        {
+            _drivers[id] = new Driver(id, position);
+        }
+    }
+
+    public Driver? GetById(string id)
+    {
+        _drivers.TryGetValue(id, out var driver);
+        return driver;
+    }
+
+    public List<Driver> GetAll()
+    {
+        return new List<Driver>(_drivers.Values);
+    }
+
+    public int Count => _drivers.Count;
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DriverMatching;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.WriteLine("Подбор водителей (начало)\n");
+
+        var store = new DriverStore();
+
+        // Добавляем несколько водителей
+        store.AddOrUpdate("D001", 10, 15);
+        store.AddOrUpdate("D002", 3, 4);
+        store.AddOrUpdate("D007", 20, 8);
+        store.AddOrUpdate("D012", 11, 14);  // обновляем или добавляем
+        store.AddOrUpdate("D005", 1, 1);
+
+        Console.WriteLine($"Всего водителей: {store.Count}");
+        Console.WriteLine("Список всех водителей:");
+
+        foreach (var driver in store.GetAll())
+        {
+            Console.WriteLine(driver);
+        }
+
+        Console.WriteLine("\nНажмите Enter для выхода...");
+        Console.ReadLine();
+    }
+}
